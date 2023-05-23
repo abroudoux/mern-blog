1649a328683a5c5fbe736b223e98651d92cc4036
abroudoux <arthur.broudoux@gmail.com>
2023-05-23 16:35:30 +0200

    form style

--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -1,14 +1,26 @@
+// React
+import { Link } from 'react-router-dom'
+
 // Styles
 import '../../styles/designs/forms.scss'
+
 
 export default function Login() {
     return (
         <section>
+            <h1>
+                Login
+            </h1>
             <form>
                 <input type="text" placeholder="username" />
                 <input type="password" placeholder="password" />
                 <button>Login</button>
             </form>
+            <div>
+                <p>
+                    Dont have an account yet ? <span><Link to="/register">Sign In</Link></span>
+                </p>
+            </div>
         </section>
     )
 }
--- client/src/components/register/Register.tsx
+++ client/src/components/register/Register.tsx
@@ -1,14 +1,26 @@
+// React
+import { Link } from 'react-router-dom'
+
 // Styles
 import '../../styles/designs/forms.scss'
+
 
 export default function Register() {
     return (
         <section>
+            <h1>
+                Register
+            </h1>
             <form>
                 <input type="text" placeholder="username" />
                 <input type="password" placeholder="password" />
                 <button>Register</button>
             </form>
+            <div>
+                <p>
+                    Already have an account ? <span><Link to="/login">Log In</Link></span>
+                </p>
+            </div>
         </section>
     )
 }
