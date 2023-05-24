d18bcde339886b21424949221de6d43cd1a0eb78
abroudoux <arthur.broudoux@gmail.com>
2023-05-25 00:52:51 +0200

    setup login

--- api/index.js
+++ api/index.js
@@ -3,7 +3,12 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const app = express();
+
+// BcryptJs
+const salt = bcrypt.genSaltSync(10);
+const secret = 'zefnzrjf5RDAZT';
 
 app.use(cors());
 app.use(express.json());
@@ -11,25 +16,36 @@ app.use(express.json());
 // MongoDB DataBase
 mongoose.connect('mongodb+srv://blog:ekViwdgqQp7d2vyk@cluster0.hrxfist.mongodb.net/?retryWrites=true&w=majority');
 
-// app.post('/register', async (req,res) => {
-//     const {username,password} = req.body;
-//     const userDoc = await User.create({ username, password })
-//     res.json(userDoc); 
-// });
-
+// Register
 app.post('/register', async (req,res) => {
+
     const {username,password} = req.body;
     try {
-        const userDoc = await User.create({ username, password })
+        const userDoc = await User.create({ 
+            username, 
+            password:bcrypt.hashSync(password,salt) })
         res.json(userDoc); 
     } catch (e) {
+        console.log(e);
         res.status(400).json(e);
     }
 });
 
-// app.get('/test', (req,res) => {
-//     res.json('test ok');
-// });
+// Login
+app.post('/login', async (req,res) => {
 
+    const {username, password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passChecked = bcrypt.compareSync(password, userDoc.password);
+
+    if (passChecked) {
+        jwt.sign({username,id:userDoc.id}, secret, {}, (err,token) => {
+            if (err) throw err;
+            res.cookie('token', token).json('ok');
+        });
+    } else {
+        req.status(400).json('wrong credentials');
+    }
+});
 
 app.listen(4000);
--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -1,19 +1,33 @@
 // React
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 // Styles
 import '../../styles/designs/forms.scss'
 
 
 export default function Login() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function loginForm(ev: { preventDefault: () => void; }) {
+        ev.preventDefault();
+        await fetch('http://localhost:4000/login', {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: {'Content-Type':'application/json'},
+        });
+    }
+
     return (
         <section>
             <h1>
                 Login
             </h1>
-            <form>
-                <input type="text" placeholder="username" />
-                <input type="password" placeholder="password" />
+            <form onSubmit={loginForm}>
+                <input type="text" placeholder="username" value={username} onChange={ev => setUsername(ev.target.value)}/>
+                <input type="password" placeholder="password" value={password} onChange={ev => setPassword(ev.target.value)}/>
                 <button>Login</button>
             </form>
             <div>
